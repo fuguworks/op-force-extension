04fa4dd84011f1b88e37a0902c2ab54dfb81fd7d
Alex Harley <alex@alexbh.dev>
2023-11-26 20:19:53 +0100

    fix: add module tag to injected

--- src/pages/content/index.ts
+++ src/pages/content/index.ts
@@ -21,6 +21,7 @@ function injectScript(file_path: string, tag: string) {
   var script = document.createElement('script');
   script.setAttribute('type', 'text/javascript');
   script.setAttribute('src', file_path);
+  script.setAttribute('type', 'module');
   node.appendChild(script);
 }
 
