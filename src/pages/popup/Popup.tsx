e1572a2d6cd0dbf470c7afe52fcfc7608868d1fd
Alex Harley <alex@alexbh.dev>
2023-11-28 12:46:27 +0100

    refactor: disabling wc and adding new landing page

--- src/pages/popup/Popup.tsx
+++ src/pages/popup/Popup.tsx
@@ -5,6 +5,7 @@ import { SessionTypes, SignClientTypes } from '@walletconnect/types';
 
 import logoColour from '@assets/img/logo-colour.svg';
 import logo from '@assets/img/logo.svg';
+import background from '@assets/img/background.png';
 import { initializeMessenger } from '@root/src/messengers';
 import { walletConnectProjectId } from '@root/src/shared/config/constants';
 import { MetamaskTransactionRequest } from '@root/src/shared/config/types';
@@ -85,135 +86,131 @@ const Popup = () => {
   const activeExtensionRequest = txs[0];
 
   return (
-    <div className="flex flex-col bg-black p-6 gap-y-4">
-      <div className="bg-white rounded-xl flex items-center justify-between py-4 pl-6 pr-6">
-        <img src={logo} style={{ width: 37.1, height: 24 }} className="" />
-        <div className="flex items-center gap-4">
-          <a href="https://twitter.com/superbridgeapp">X.COM</a>
-          <a>DOCS</a>
-        </div>
-      </div>
-
-      {activeExtensionRequest || activeWcRequest ? (
-        <div className="flex items-center flex-col bg-white rounded-xl pt-12 px-6 pb-8 gap-y-6">
-          <img src={logoColour} style={{ width: 136.02, height: 88 }} className="" />
-          <div className="text-center text-lg font-bold">
-            Would you like to use the Escape Hatch for this transaction?
-          </div>
-
-          <div className="flex items-center gap-4 font-bold w-full">
-            <button
-              className="py-4 px-6 rounded-full ring-2 ring-zinc-900 ring-inset w-full"
-              onClick={() =>
-                activeExtensionRequest
-                  ? onMetaMaskClick(activeExtensionRequest, true)
-                  : onWalletConnectClick(activeWcRequest, true)
-              }>
-              No
-            </button>
-            <button
-              className="py-4 px-6 bg-zinc-900 text-white rounded-full w-full"
-              onClick={() =>
-                activeExtensionRequest
-                  ? onMetaMaskClick(activeExtensionRequest, false)
-                  : onWalletConnectClick(activeWcRequest, false)
-              }>
-              Yes
-            </button>
+    <div className="relative h-full">
+      <img src={background} className="absolute h-full w-full" />
+      <div className="relative z-10 flex flex-col  p-6 gap-y-4">
+        <div className="bg-white rounded-xl flex items-center justify-between py-4 pl-6 pr-6">
+          <img src={logo} style={{ width: 37.1, height: 24 }} className="" />
+          <div className="flex items-center gap-4">
+            <a href="https://twitter.com/superbridgeapp">X.COM</a>
+            <a>DOCS</a>
           </div>
         </div>
-      ) : (
-        <div className="flex items-center flex-col bg-white rounded-xl pt-12 px-6 pb-8 gap-y-6">
-          <img src={logoColour} style={{ width: 136.02, height: 88 }} className="" />
-          <div className="text-center text-lg font-bold">
-            Connect to the
-            <br />
-            Escape Hatch
-          </div>
 
-          <div className="flex items-center gap-4 font-bold w-full">
-            <button
-              className="py-4 px-6 bg-zinc-900 text-white rounded-full w-full"
-              onClick={() => messenger.send('init', {})}>
+        {activeExtensionRequest || activeWcRequest ? (
+          <div className="flex items-center flex-col bg-white rounded-xl pt-12 px-6 pb-8 gap-y-6">
+            <img src={logoColour} style={{ width: 136.02, height: 88 }} className="" />
+            <div className="text-center text-lg font-bold">
+              Would you like to use the Escape Hatch for this transaction?
+            </div>
+
+            <div className="flex items-center gap-4 font-bold w-full">
+              <button
+                className="py-4 px-6 rounded-full ring-2 ring-zinc-900 ring-inset w-full"
+                onClick={() =>
+                  activeExtensionRequest
+                    ? onMetaMaskClick(activeExtensionRequest, true)
+                    : onWalletConnectClick(activeWcRequest, true)
+                }>
+                No
+              </button>
+              <button
+                className="py-4 px-6 bg-zinc-900 text-white rounded-full w-full"
+                onClick={() =>
+                  activeExtensionRequest
+                    ? onMetaMaskClick(activeExtensionRequest, false)
+                    : onWalletConnectClick(activeWcRequest, false)
+                }>
+                Yes
+              </button>
+            </div>
+          </div>
+        ) : (
+          <div className="flex items-center flex-col rounded-xl py-12 px-6 gap-y-6">
+            <img src={logoColour} style={{ width: 136.02, height: 88 }} className="" />
+            <div className="text-center text-lg font-bold">
+              Force L2 transactions
+              <br />
+              via Ethereum
+            </div>
+          </div>
+        )}
+
+        <div className="bg-white rounded-xl p-4 gap-2">
+          <div>Pending transactions</div>
+
+          {[...txs, ...wcRequests].length === 0 ? (
+            <div className="text-zinc-400">No transactions yet…</div>
+          ) : (
+            <>
+              {[...txs, ...wcRequests].map((p, index) => (
+                <div className="flex items-center gap-2 bg-blue-100 rounded-full w-full">
+                  <Loading />
+                  <div className="font-bold text-xs text-blue-500 p-2 pr-3">Decoded tx info</div>
+                </div>
+              ))}
+            </>
+          )}
+        </div>
+
+        {/* <div className="bg-white rounded-xl p-4 gap-2">
+          <div>Connect WC</div>
+
+          <div>
+            <input
+              type="text"
+              className="bg-zinc-100 w-full rounded-md p-2"
+              value={uri}
+              onChange={e => setUri(e.target.value)}
+            />
+            <button className="py-4 px-6 bg-zinc-900 text-white rounded-full w-full" onClick={onConnectWc}>
               Connect
             </button>
           </div>
+        </div> */}
+
+        {/* <div className="bg-white rounded-xl p-4 gap-2">
+          <div>Current connections</div>
+
+          {wcSessions.length === 0 ? (
+            <div className="text-zinc-400">No connected wallets yet…</div>
+          ) : (
+            <>
+              {wcSessions.map(s => (
+                <div className="flex bg-zinc-100 p-2 items-center gap-2 rounded-full  ">
+                  <img className="h-4 w-4 rounded-full" src={s.peer.metadata.icons[0]} />
+                  <div>{s.peer.metadata.name}</div>
+
+                  <button className="ml-auto" onClick={() => onDisconnectWallet(s)}>
+                    X
+                  </button>
+                </div>
+              ))}
+            </>
+          )}
+        </div> */}
+
+        {/* <div className="bg-white rounded-xl p-4 gap-2">
+          <div>Connected apps</div>
+
+          {dapps.length === 0 ? (
+            <div className="text-zinc-400">No connected apps yet…</div>
+          ) : (
+            <>
+              {dapps.map(s => (
+                <div className="flex bg-zinc-100 p-2 items-center gap-2 rounded-full  ">
+                  <img className="h-4 w-4 rounded-full" src={s.peer.metadata.icons[0]} />
+                  <div>{s.peer.metadata.name}</div>
+
+                  <button className="ml-auto" onClick={() => onDisconnectApp(s)}>
+                    X
+                  </button>
+                </div>
+              ))}
+            </>
+          )}
         </div>
-      )}
-
-      <div className="bg-white rounded-xl p-4 gap-2">
-        <div>Pending transactions</div>
-
-        {[...txs, ...wcRequests].length === 0 ? (
-          <div className="text-zinc-400">No transactions yet…</div>
-        ) : (
-          <>
-            {[...txs, ...wcRequests].map((p, index) => (
-              <div className="flex items-center gap-2 bg-blue-100 rounded-full w-full">
-                <Loading />
-                <div className="font-bold text-xs text-blue-500 p-2 pr-3">Decoded tx info</div>
-              </div>
-            ))}
-          </>
-        )}
-      </div>
-
-      <div className="bg-white rounded-xl p-4 gap-2">
-        <div>Connect WC</div>
-
-        <div>
-          <input
-            type="text"
-            className="bg-zinc-100 w-full rounded-md p-2"
-            value={uri}
-            onChange={e => setUri(e.target.value)}
-          />
-          <button className="py-4 px-6 bg-zinc-900 text-white rounded-full w-full" onClick={onConnectWc}>
-            Connect
-          </button>
-        </div>
-      </div>
-
-      <div className="bg-white rounded-xl p-4 gap-2">
-        <div>Current connections</div>
-
-        {wcSessions.length === 0 ? (
-          <div className="text-zinc-400">No connected wallets yet…</div>
-        ) : (
-          <>
-            {wcSessions.map(s => (
-              <div className="flex bg-zinc-100 p-2 items-center gap-2 rounded-full  ">
-                <img className="h-4 w-4 rounded-full" src={s.peer.metadata.icons[0]} />
-                <div>{s.peer.metadata.name}</div>
-
-                <button className="ml-auto" onClick={() => onDisconnectWallet(s)}>
-                  X
-                </button>
-              </div>
-            ))}
-          </>
-        )}
-      </div>
-
-      <div className="bg-white rounded-xl p-4 gap-2">
-        <div>Connected apps</div>
-
-        {dapps.length === 0 ? (
-          <div className="text-zinc-400">No connected apps yet…</div>
-        ) : (
-          <>
-            {dapps.map(s => (
-              <div className="flex bg-zinc-100 p-2 items-center gap-2 rounded-full  ">
-                <img className="h-4 w-4 rounded-full" src={s.peer.metadata.icons[0]} />
-                <div>{s.peer.metadata.name}</div>
-
-                <button className="ml-auto" onClick={() => onDisconnectApp(s)}>
-                  X
-                </button>
-              </div>
-            ))}
-          </>
-        )}
+      */}
       </div>
     </div>
   );
